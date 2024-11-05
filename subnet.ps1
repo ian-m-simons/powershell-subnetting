29c8e490e7ddb1d595d01f9902a388c564e3adde
Ian Simons <iansimons93@gmail.com>
2024-11-05 11:40:46 -0500

    adding creation of dhcp scopes

--- subnet.ps1
+++ subnet.ps1
@@ -8,6 +8,8 @@ function Subnet-ByNetworkCount24{
         $addedBits++
     }
     $cleanIP = $splitNetMask[0]
+    $ListOfSubnets = @()
+
     if ($importantOctet -eq "3"){
         $octet = @("","","","")
         $delimiter = 0
@@ -24,67 +26,141 @@ function Subnet-ByNetworkCount24{
         $addressCount = [Math]::Pow(2,8-$addedBits)
         write-host "Your subnet ID's and masks (in CIDR notation) are listed below, each subnet will contain " $addressCount " total addresses (only " ($addressCount - 2) " are useable)"
         for ($i = 0; $i -lt 255; $i += ([Math]::Pow(2,8-$addedBits))){
-            write-Host $octet[0]"."$octet[1]"."$octet[2]"."$i"/"$newNetMask
+           # write-Host $octet[0]'.'$octet[1]'.'$octet[2]'.'$i'/'$newNetMask
+            $temp = $octet[0] + '.' + $octet[1] + '.' + $octet[2] + '.' + [string]$i
+            $ListOfSubnets += $temp
+
+        }
+        for($i = 0; $i -lt $ListOfSubnets.Length; $i++){
+            write-host $ListOfSubnets[$i]'/'$newNetMask
         }
     }
-    elseif ($importantOctet -eq "2"){
+
+    if ($importantOctet -eq "2"){
         $octet = @("","","","")
         $delimiter = 0
-        for ($i = 0; $i -lt $cleanIP.Length; $i++){
-            if ($cleanIP[$i] -eq "."){
+        for ($i = 0; $i -lt $splitNetMask[0].Length; $i++){
+            if ($splitNetMask[0][$i] -eq "."){
                 $delimiter++
             }
             else{
-                $octet[$delimiter]+= $cleanIP[$i]
+                $octet[$delimiter]+= $splitNetMask[0][$i]
             }
-        }
-        for ($i = 0; $i -lt $octet.Length-1; $i++){
-            $octet[$i] += " ."
         }
 
         $newNetMask = $addedBits+[int]$splitNetMask[1]
         $addressCount = [Math]::Pow(2,8-$addedBits)*256
         write-host "Your subnet ID's and masks (in CIDR notation) are listed below, each subnet will contain " $addressCount " total addresses (only " ($addressCount - 2) " are useable)"
         for ($i = 0; $i -lt 255; $i += ([Math]::Pow(2,8-$addedBits))){
-            $octet[2] = [string]$i 
-            $octet[2] += " ."
-            write-host $octet` `/$newNetMask
+           # write-Host $octet[0]'.'$octet[1]'.'$octet[2]'.'$i'/'$newNetMask
+            $temp = $octet[0] + '.' + $octet[1] + '.' + [string]$i + '.' + $octet[3]
+            $ListOfSubnets += $temp
+
         }
-        
+        for($i = 0; $i -lt $ListOfSubnets.Length; $i++){
+            write-host $ListOfSubnets[$i]'/'$newNetMask
+        }
     }
-    elseif ($importantOctet -eq "1"){
+
+    if ($importantOctet -eq "1"){
         $octet = @("","","","")
         $delimiter = 0
-        for ($i = 0; $i -lt $cleanIP.Length; $i++){
-            if ($cleanIP[$i] -eq "."){
+        for ($i = 0; $i -lt $splitNetMask[0].Length; $i++){
+            if ($splitNetMask[0][$i] -eq "."){
                 $delimiter++
             }
             else{
-                $octet[$delimiter]+= $cleanIP[$i]
+                $octet[$delimiter]+= $splitNetMask[0][$i]
             }
-        }
-        for ($i = 0; $i -lt $octet.Length-1; $i++){
-            $octet[$i] += " ."
         }
 
         $newNetMask = $addedBits+[int]$splitNetMask[1]
-        $addressCount = [Math]::Pow(2,8-$addedBits)*[Math]::Pow(256,2)
+        $addressCount = [Math]::Pow(2,8-$addedBits)*[Math]::Pow(256, 2)
         write-host "Your subnet ID's and masks (in CIDR notation) are listed below, each subnet will contain " $addressCount " total addresses (only " ($addressCount - 2) " are useable)"
         for ($i = 0; $i -lt 255; $i += ([Math]::Pow(2,8-$addedBits))){
-            $octet[1] = [string]$i 
-            $octet[1] += " ."
-            write-host $octet` `/$newNetMask
+           # write-Host $octet[0]'.'$octet[1]'.'$octet[2]'.'$i'/'$newNetMask
+            $temp = $octet[0] + '.' + [string]$i + '.' + $octet[2] + '.' + $octet[3]
+            $ListOfSubnets += $temp
+
         }
-        
+        for($i = 0; $i -lt $ListOfSubnets.Length; $i++){
+            write-host $ListOfSubnets[$i]'/'$newNetMask
+        }
     }
-}
-function Subnet-ByAddressCount{
-    param($splitNetMask, $desiredAddressCount)
     
 
-
+    $makeScope = read-host "Would you like to use the created subnets to make a new scope? (1=yes, 0=no)"   
+    $makeScope = [int]$makeScope
+    if ($makeScope -eq 0 ){
+        exit
+    }
+    elseif ($makeScope -eq 1){
+        Offer-DHCPv4Scope -subnetList $ListOfSubnets -CIDRMask $newNetMask
+    }
+    else{
+        write-host "[PEBCAK ERROR] exiting program - seek help from administrator and try again"
+    }
+    
+    
+    
+    
+    
 }
-write-host "Warning this program currently only works for /24 networks`nLater as time allows I will expand functionality"
+
+function Offer-DHCPv4Scope{
+    param([array]$subnetList, [int]$CIDRMask)
+    
+    $SubnetMask = ''
+    $FilledOctets = [Math]::truncate($CIDRMask/8)
+    for ($i = 0; $i -lt $FilledOctets; $i++){
+        $SubnetMask += '255.'
+    }
+    $totallyMeaningfulVariable = $FilledOctets+1
+    $NoteworthyOctet = 0
+    for ($i = 0; $i -lt 255; $i += [Math]::Pow(2,(8-($CIDRMask % 8)))){
+        $NoteWorthyOctet = $i
+    }
+    $SubnetMask += [string]$NoteWorthyOctet
+    $FilledOctets++
+    if ($FilledOctets -lt 4){
+        for ($i = $FilledOctets; $i -lt 4; $i++){
+            $SubnetMask += '.0'
+        }
+    }
+    $numberOfScopes = Read-Host "how many scopes would you like to create"
+    if ($numberOfScopes -gt $subnetList.Length){
+        write-host "You're a dumbass, go get your boss and try again"
+        exit
+    }
+    write-host $numberOfScopes
+    for($i = 0; $i -lt $numberOfScopes; $i++){
+        write-host "creating scope number" $i
+        $scopeName = Read-Host "what would you like to name this scope?"
+        $startScopeRange = $subnetList[$i]
+        $startScopeRangeList = $startScopeRange.split('.')
+        $startScopeRangeList[$totallyMeaningfulVariable - 1] = [int]$startScopeRangeList[$totallyMeaningfulVariable - 1] +1
+        $startScopeRange = ""
+        if($i+1 -lt $subnetList.Length){
+            $endScopeRange = $subnetList[$i+1]
+            $endScopeRangeList = $endScopeRange.split('.')
+            $endScopeRangeList[$totallyMeaningfulVariable -1] = [int]$endScopeRangeList[$totallyMeaningfulVariable - 1] -2
+            $endScopeRange = ""
+        }
+        else{
+            write-host 'you need to write this edge case still'
+        }
+        for ($j = 0; $j -lt 4; $j++){
+            $startScopeRange += $startScopeRangeList[$j]
+            $endScopeRange += $endScopeRangeList[$j] 
+            if ($j -lt 3){
+                $startScopeRange += '.'
+                $endScopeRange += '.'
+            }
+        }
+        
+        write-host  'Add-DhcpServerv4Scope -Name' $scopeName '-StartRange' $startScopeRange '-EndRange' $endScopeRange '-SubnetMask' $SubnetMask
+    }
+}
 $CurrentNetwork = Read-Host "please enter the current subnetID and subnet mask in CIDR notation`n(for example 192.168.0.0/24)"
 $splitNetMask = $CurrentNetwork -split "/"
 $importantOctet = 0
@@ -101,6 +177,7 @@ else{
     write-host "error please try again"
     exit
 }
+
 
 $desiredNetworkCountRaw = Read-Host "please enter the number of networks you would like total when subnetting is completed"
 $desiredNetworkCount = [int]$desiredNetworkCountRaw
